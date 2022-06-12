efc9235735498b66421a8c137841e45f8edb3003
bociwess0 <dimitrijeveljkovic2@gmail.com>
2022-06-12 17:12:19 +0200

    Add model Product and migration

--- be-laravel/database/migrations/2022_06_12_150604_create_products_table.php
+++ be-laravel/database/migrations/2022_06_12_150604_create_products_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('categoryName');
+            $table->string('productName');
+            $table->string('ram');
+            $table->string('size');
+            $table->string('color');
             $table->timestamps();
         });
     }
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('products');
     }
 };
