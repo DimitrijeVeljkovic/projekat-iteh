3fe838dd508e4f1d62797d8924134c3f918e2433
bociwess0 <dimitrijeveljkovic2@gmail.com>
2022-06-18 21:21:23 +0200

    Add dark mode and fix some styles

--- fe-angular/src/app/app.component.ts
+++ fe-angular/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fe-angular';
+
+  @HostBinding('class') className = '';
+
+  ngOnInit() {
+  }
+
+  public toggleChanged(checked: boolean) {
+    this.className = checked ? 'dark-mode' : '';
+  }
 }
--- fe-angular/src/app/components/footer/footer.component.ts
+++ fe-angular/src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  @Output() toggleChange: EventEmitter<boolean> = new EventEmitter<boolean>(false);
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public handleToggleChange(event: MatSlideToggleChange) {
+    this.toggleChange.emit(event.checked);
+  }
 }
