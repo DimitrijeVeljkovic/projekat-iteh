5dd70de24644bd02846978d611d5a93f8c1761c5
bociwess0 <bogdandjordjevic2000@gmail.com>
2022-06-24 22:54:51 +0200

    Design for change user data

--- fe-angular/src/app/components/profile/profile.component.ts
+++ fe-angular/src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -8,17 +9,38 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class ProfileComponent implements OnInit {
   public userData: any;
+  public changeDataForm: FormGroup;
 
-  constructor(private _userService: UserService) { }
+  constructor(private _userService: UserService,
+              private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.getUserDataFromService().subscribe(result => {
-      this.userData = result
+      this.userData = result;
+      console.log(this.userData);
     });
+
+    this.changeDataForm = this._formBuilder.group({
+      email: ['', [Validators.email]],
+      name: ['']
+    });
+  }
+
+  public get validForm() {
+    console.log(this.changeDataForm);
+    return this.changeDataForm.valid;
+  }
+
+  public get invalidEmail() {
+    const emailControl = this.changeDataForm.get('email');
+    if (emailControl !== null) {
+      if (emailControl.errors) {
+        return emailControl.errors['email'] && emailControl.touched;
+      }
+    }
   }
 
   public getUserDataFromService() {
     return this._userService.userData();
   }
-
 }
